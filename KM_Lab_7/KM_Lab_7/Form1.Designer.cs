b4dea861b367c29d478f5e22e6820071a7e123d3
IvanDerda <115139625+IvanDerda@users.noreply.github.com>
2023-05-03 09:34:48 +0300

    Test
    Lab_7

---
+++ KM_Lab_7/KM_Lab_7/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace KM_Lab_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.FlagIMG = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.FlagIMG)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MV Boli", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(226, 301);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Знайти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(317, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(215, 29);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 24);
+            this.label1.TabIndex = 2;
+            // 
+            // FlagIMG
+            // 
+            this.FlagIMG.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.FlagIMG.Location = new System.Drawing.Point(364, 77);
+            this.FlagIMG.Name = "FlagIMG";
+            this.FlagIMG.Size = new System.Drawing.Size(298, 195);
+            this.FlagIMG.TabIndex = 3;
+            this.FlagIMG.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(103, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(208, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Введіть назву країни :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(663, 364);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.FlagIMG);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.FlagIMG)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox FlagIMG;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ KM_Lab_7/KM_Lab_7/Form1.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+using System.Drawing;
+using System.IO;
+
+using System.Net;
+
+using System.Windows.Forms;
+
+namespace KM_Lab_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var _name = textBox1.Text;
+
+            var request = new GetRequest("https://restcountries.com/v3.1/name/"+_name);
+            request.Run();
+
+            var response = request.Response;
+           
+            if(response != null)
+            {
+                JArray j_arr = JArray.Parse(response);
+                JObject j_obj = j_arr[0].ToObject<JObject>();
+                string country_name = j_obj["name"]["official"].ToString();
+                string country_capital = j_obj["capital"][0].ToString();
+                string country_reg = j_obj["region"].ToString();
+                string country_continents = j_obj["continents"][0].ToString();
+                string imageUrl = j_obj["flags"]["png"].ToString();
+                label1.Text = 
+                    "Країна: " + country_name + "\n" + 
+                    "Столиця: " + country_capital + "\n" + 
+                    "Регіон: " + country_reg + "\n" +
+                    "Континент: " + country_continents;
+                WebClient client = new WebClient();
+                Stream stream = client.OpenRead(imageUrl) ;
+                Bitmap bitmap = new Bitmap(stream);
+                FlagIMG.Image= bitmap;
+                FlagIMG.BackgroundImageLayout = ImageLayout.Stretch;
+                
+            }
+            else
+            {
+                label1.Text = "Помилка:\n" +
+                    "- Назву вводити латиницею\n" +
+                    "- Перевірьте введене\n" +
+                    "- Країни не існує";
+                FlagIMG.Image = null;
+            }
+            
+            
+
+            
+            
+            
+
+        }
+    }
+}
